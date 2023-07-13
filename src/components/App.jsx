ecee371e0d91194061af3905c304109d9a86eb48
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-13 22:59:34 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,16 +18,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Statistics" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+
     </div>
   );
 };

5cda5e9eb4ae9deeb69ce2c9b68c31439f4b76c8
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-13 23:28:31 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,12 @@
+import { Profile } from "./Profile.jsx";
+import { Statistics } from "./Statistics.jsx";
+import { FriendList } from "./FriendList.jsx";
+import { TransactionHistory } from "./TransactionHistory.jsx";
 
+import user from './json/user.json';
+import data from './json/data.json';
+import friends from './json/friends.json';
+import transactions from './json/transactions.json';
 
 export const App = () => {
   return (
@@ -9,7 +17,16 @@ export const App = () => {
         color: '#010101'
       }}
     >
-
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Statistics" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };

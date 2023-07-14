415fc10489aff8ead8e9fd3ca7b9b077b127bb1c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-14 21:37:30 +0300

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,11 +2,11 @@ import css from "./FriendList.module.css";
 import general from "../General.module.css";
 import propTypes from 'prop-types';
 
-export const FriendListItem = ({}) => {
-    <li className={`${css.friendListItem} ${general.item}`} key={friend.id}>
-        <span className={`${css.status} ${friend.isOnline ? css.online : css.offline}`}></span>
-        <img className={css.avatar} src={friend.avatar} alt={friend.name} width="48" />
-        <p className={css.name}>{friend.name}</p>
+export const FriendListItem = ({avatar, name, isOnline}) => {
+    <li className={`${css.friendListItem} ${general.item}`}>
+        <span className={`${css.status} ${isOnline ? css.online : css.offline}`}></span>
+        <img className={css.avatar} src={avatar} alt={name} width="48" />
+        <p className={css.name}>{name}</p>
     </li>
 }
 

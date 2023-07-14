f3b69f68d50f29cc77df40a54b22ada433081fb1
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-14 18:20:13 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -11,22 +11,21 @@ export const Statistics = ({title, stats}) => {
         titleString = <h2 className={css.title}>{title}</h2>;
     }
 
-    let statListItems = [];
-    stats.forEach(statsItem => {
-        statListItems.push(
-            <li className={css.statisticsItem + " " + general.item} key={statsItem.id}>
-                <span className={css.label}>{statsItem.label}</span>
-                <span className={css.percentage}>{statsItem.percentage}%</span>
-            </li>
-        );
-    });
-
     return (
         <section className={css.statistics + " " + general.container}>
             {titleString}
 
             <ul className={css.statList}>
-                {statListItems}
+                {
+                    stats.map(statsItem => 
+                        (
+                            <li className={css.statisticsItem + " " + general.item} key={statsItem.id}>
+                                <span className={css.label}>{statsItem.label}</span>
+                                <span className={css.percentage}>{statsItem.percentage}%</span>
+                            </li>
+                        )
+                    )
+                }
             </ul>
         </section>
     );
